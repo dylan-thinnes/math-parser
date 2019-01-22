f84285256df29271dc7ad956be83e94431f19ad4
dylan-thinnes <dylan.thinnes@protonmail.com>
2019-01-22 20:31:22 +0000

    Create basic operator / expression datatypes

--- MathParse.hs
+++ MathParse.hs
@@ -1,1 +1,17 @@
 module MathParse where
+
+data Expr = Num Integer
+          | Expr :**: Expr
+          | Expr :*: Expr
+          | Expr :/: Expr
+          | Expr :+: Expr
+          | Expr :-: Expr
+    deriving (Eq, Show, Read)
+
+data Operator = Subtract
+              | Add
+              | Divide
+              | Times
+              | Exponentiate
+              | OpenParen | CloseParen
+    deriving (Eq, Enum, Ord, Bounded)
