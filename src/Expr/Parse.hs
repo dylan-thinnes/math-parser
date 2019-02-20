1dbca2cc4be0239b4ede6df96fc2541a3c3619cd
dylan-thinnes <dylan.thinnes@protonmail.com>
2019-02-20 21:09:22 +0000

    Move right/left assoc & pre/post to Expr.Parse

--- src/Expr/Parse.hs
+++ src/Expr/Parse.hs
@@ -1,1 +1,14 @@
 module Expr.Parse where
+
+-- Binary Operators
+rassoc, lassoc :: BinOp -> Bool
+rassoc Exponentiate = True
+rassoc _            = False
+lassoc = not . rassoc
+
+-- Unary Operators
+pre, post :: UnOp -> Bool
+pre Negate    = True
+pre Not       = True
+pre Factorial = False
+post = not . pre
