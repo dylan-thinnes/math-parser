7355b1173f55cbc61736f9379ed9f49bc75b6f76
dylan-thinnes <dylan.thinnes@protonmail.com>
2019-02-20 22:25:15 +0000

    Add rop for reading ops & toTableEntry for CExpr

--- src/Expr/Parse.hs
+++ src/Expr/Parse.hs
@@ -11,6 +11,12 @@ class (Show a) => Operator a where
     expr :: a -> Expr -> Expr -> Expr
     wrap :: (Monad m) => a -> (m (Expr -> Expr -> Expr)) -> CExpr.Operator m Expr
 
+rop :: (Operator a) => a -> ReadP a
+rop op = choice $ map (\s -> op <$ (skipSpaces >> string s)) $ symbols op
+
+toTableEntry :: (Operator a) => a -> CExpr.Operator ReadP Expr
+toTableEntry op = wrap op $ expr op <$ rop op
+
 -- Binary Operators
 rassoc, lassoc :: BinOp -> Bool
 rassoc Exponentiate = True
